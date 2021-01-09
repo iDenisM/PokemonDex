5abb6bf9bdb873fb55ecd17fd08d494bacfd9965
Denis Mitiliuc <denis.mitiliuc@gmail.com>
2021-01-09 20:57:12 +0100

    test fetch data

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
-import Button from './components/Button'
+import Button from './components/Button';
+import { useQuery, gql } from '@apollo/client';
+
+const ALL_POKEMONS = gql`
+  {
+    pokemons(first: 10){
+      id
+      name
+      types
+      image
+    }
+  }
+`
 
 function App() {
+  const { loading, error, data } = useQuery(ALL_POKEMONS);
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <h2>Whoops... somthing went wrong!</h2>
+
+  console.log(data);
+
   return (
     <>
-      <div className="temp">Hello Pokemon Player</div>
       <Button>Hello</Button>
+      <header></header>
+      <main>
+        {
+          data.pokemons.map(p => (
+            <div key={p.id}>
+              <img src={p.image} alt={p.name}></img>
+              <div>{p.name}</div>
+            </div>
+          ))
+        }
+      </main>
+      <footer></footer>
     </>
   );
 }
