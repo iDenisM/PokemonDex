df1ce15c06f80abc2751d567acb0133384636b0b
Denis Mitiliuc <denis.mitiliuc@gmail.com>
2021-01-09 18:18:21 +0100

    more cleaning before start

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Button.css';
 
-const Button = ({ children, image, ...rest }) => {
+const Button = ({ children, ...rest }) => {
   return (
     <button className="btn" {...rest}>
       { children }
@@ -11,7 +11,10 @@ const Button = ({ children, image, ...rest }) => {
 }
 
 Button.propTypes = {
-  label: PropTypes.any
+  children: PropTypes.any
 }
+
+// Button.defaultProps = {
+// };
 
 export default Button;
