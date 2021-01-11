9f90777dfefa3012a2e658c481adbe2657b6c518
Denis Mitiliuc <denis.mitiliuc@gmail.com>
2021-01-11 13:54:23 +0100

    wokring on linking the game logic to worker

--- client/public/workers/Engine.js
+++ client/public/workers/Engine.js
@@ -1,3 +1,10 @@
-onmessage = e => {
+import GameState from '../../src/game/GameState';
+
+new GameState();
+
+self.onmessage = async e => {
   console.log(e.data.msg);
+  if (e.data.msg === 'ADD_BOT') {
+
+  }
 }
--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -8,13 +8,13 @@ export default class Board extends React.Component {
     this.worker = new Worker('./workers/Engine.js');
     this.worker.onmessage = (event) => {
       if (event && event.data) {
-        console.log(event.data);
+        console.log('away', event.data);
       }
     };
   }
 
   isClicked = () => {
-    this.worker.postMessage({msg: 'asdfasdfasdfasdf'})
+    this.worker.postMessage({msg: 'ADD_BOT', value: 1})
   }
 
   render() {
