f87a838c57ef64b4c690375156f107b8b4de02f9
Denis Mitiliuc <denis.mitiliuc@gmail.com>
2021-01-13 09:31:53 +0100

    style the search

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,16 +1,22 @@
+import './Search.css';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { searchKeyDown } from '../../actions'
 
 const Search = () => {
   const dispatchKeyDown = useDispatch();
-
+  const searchLabel = 'Search Pokemon';
   return (
-    <input 
-      className="search" 
-      type="text" 
-      onKeyUp={e => dispatchKeyDown(searchKeyDown(e.currentTarget.value))} 
-    />
+    <div className="search">
+      <label className="is-vHidden" htmlFor="search-pokemon">{searchLabel}</label>
+      <input 
+        id="search-pokemon"
+        className="search__input" 
+        type="text" 
+        placeholder={searchLabel}
+        onKeyUp={e => dispatchKeyDown(searchKeyDown(e.currentTarget.value))} 
+      />
+    </div>
   )
 }
 
